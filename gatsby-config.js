13fde0783af4f125f9700f37d9ef88cec58c122c
thebigsasha <sashathethird@gmail.com>
2021-07-29 23:56:23 -0400

    Update branding

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,10 @@ require("dotenv").config({
 module.exports = {
   pathPrefix: "/sasharesume-next",
   siteMetadata: {
-    title: `Sashaphoto Gatsby Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `sasharesume`,
+    description: `Alexander Aleshchenko's online resume for software and engineering projects.`,
+    author: `Alexander Aleshchenko - GitHub/TheBigSasha`,
+    siteUrl: `sasharesume.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -29,8 +29,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#00ffc8`,
+        theme_color: `#00ffc8`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
